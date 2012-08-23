6d88874ca0f2783851cec148e6eb8cd6429a80bd
James Choi <james.choiboi@gmail.com>
2012-08-23 07:36:40 -0400

    Reorganize repository. Added folder for Android version.

---
+++ Android/js/applicationFunctions.js
@@ -0,0 +1,88 @@
+// Close the application.
+function closeApp() {
+	blackberry.app.exit();
+}
+
+// Get the current system date.
+function dateParser() {
+	var dateRaw = new Date();
+	var dateArray = dateRaw.toString().split(" ");
+	var date =  dateArray[1] + ". " + dateArray[2] + ", " + dateArray[3];
+	return date;
+}
+
+// Get the index value from the URL
+function getIndexValue(url) {
+	var indexStr = url.substring(url.indexOf("?") + 1, url.length);
+	var index = indexStr.substring(indexStr.indexOf("=") + 1, indexStr.length);
+	
+	return parseInt(index);
+}
+
+// Add #home tage in the URL when the application first loads.
+// This will enable it to list all the TODOs.
+function loadPage() {
+	var currentPage = $(location).attr("href");
+	
+	if (currentPage.indexOf("#home") === -1 && 
+			currentPage.indexOf("#") === -1 ) {
+		window.location = currentPage + "#home";
+	}
+}
+
+// Open up the browser application to the specified link.
+function openBrowser(link) {
+	var args = new blackberry.invoke.BrowserArguments(link);
+	blackberry.invoke.invoke(blackberry.invoke.APP_BROWSER, args);
+}
+
+// Display alert indicating that title field or the text field is empty.
+function alertEmptyText(title, text) {
+	var INFO_TEXT = "Please enter information.",
+		settings = {size : blackberry.ui.dialog.SIZE_SMALL,
+					position : blackberry.ui.dialog.CENTER};
+	
+	if ($(title).val() === "" && $(text).val() === "") {
+		blackberry.ui.dialog.standardAskAsync("Both Title and TODO fields empty.\n" + INFO_TEXT,
+											  blackberry.ui.dialog.D_OK,
+											  function () {},
+											  settings);
+	} else if ($(title).val() === "") {
+		blackberry.ui.dialog.standardAskAsync("Title field empty.\n" + INFO_TEXT,
+											  blackberry.ui.dialog.D_OK,
+											  function () {},
+											  settings);
+	} else {
+		blackberry.ui.dialog.standardAskAsync("TODO field empty.\n" + INFO_TEXT,
+											  blackberry.ui.dialog.D_OK,
+											  function () {},
+											  settings);
+	}
+}
+
+// TextAreaResizer plugin.
+$.fn.TextAreaResizer = function() {
+	var textarea, staticOffset,
+		iLastMousePos = 0;
+
+
+	return this.each(function() {
+		textarea = $(this).addClass('processed'), staticOffset = null;
+		
+		$(this).wrap('<div class="resizable-textarea"><span></span></div>')
+			.parent().append($('<div class="grippie"></div>').bind("mousedown",{el: this} , startDrag));
+		
+		var grippie = $('div.grippie', $(this).parent())[0];
+		grippie.style.marginRight = (grippie.offsetWidth - $(this)[0].offsetWidth) +'px';
+	});
+	
+	function startDrag(e) {
+		textarea = $(e.data.el);
+		textarea.blur();
+		iLastMousePos = mousePosition(e).y;
+		staticOffset = textarea.height() - iLastMousePos;
+		textarea.css('opacity', 0.25);
+		$(document).mousemove(performDrag).mouseup(endDrag);
+		return false;
+	}
+}
---
+++ Android/js/storageFunctions.js
@@ -0,0 +1,75 @@
+// Adds note into localStorage.
+function addNote() {
+	if ($("#addNoteTitle").val() === "" ||
+			$("#addNoteText").val() === "" ) {
+		alertEmptyText("#addNoteTitle", "#addNoteText");
+	} else {
+		var date = dateParser();
+		
+		var data = JSON.stringify({
+			"title" : $("#addNoteTitle").val(), 
+			"note" : $("#addNoteText").val(), 
+			"dateAdded" : date,
+			"completed" : "false"
+		});
+
+		localStorage.setItem(localStorage.length, data);
+		$.mobile.changePage("/index.html#home", { transition: "none"});
+	}
+}
+
+// Delete specified note from localStorage.
+function deleteNote() {
+	var index = getIndexValue(document.URL),
+		ind = 0;
+	
+	for (ind = index; ind < localStorage.length; ind++) {
+		if (ind + 1 < localStorage.length) {
+			nextData = localStorage.getItem(ind + 1);
+			localStorage.setItem(ind, nextData);
+		} else if (ind === localStorage.length - 1) {
+			localStorage.removeItem(ind);
+		}
+	}
+}
+
+// Edit note title and TODOs with newly provided data.
+function editNote() {
+	if ($("#editNoteTitle").val() === "" ||
+			$("#editNoteText").val() === "" ) {
+		alertEmptyText("#editNoteTitle", "#editNoteText");
+	} else {
+		var	index = getIndexValue(document.URL);
+		var dataJSON = jQuery.parseJSON(localStorage.getItem(index));
+		
+		var data = JSON.stringify({
+			"title" : $("#editNoteTitle").val(), 
+			"note" : $("#editNoteText").val(),  
+			"dateAdded" : dataJSON.dateAdded,
+			"completed" : dataJSON.completed
+		});
+		
+		localStorage.setItem(index, data);
+		$.mobile.changePage("/index.html#home", { transition: "none"});
+	}
+}
+
+// Change TODO status (either completed or not completed).
+function markCompleted() {
+	var index = getIndexValue($(location).attr("href")),
+		marked = "false";
+	var dataJSON = jQuery.parseJSON(localStorage.getItem(index));
+	
+	if (dataJSON.completed === "false") {
+		marked = "true";
+	}
+	
+	var data = JSON.stringify({
+		"title" : dataJSON.title, 
+		"note" : dataJSON.note, 
+		"dateAdded" : dataJSON.dateAdded,
+		"completed" : marked
+	});
+	
+	localStorage.setItem(index, data);
+}
